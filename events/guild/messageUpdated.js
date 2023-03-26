e730a8eaaeac05881e5e93e9c5bedbd61c12272d
ForGetFulSkyBro <forgetfulskybro9@gmail.com>
2023-03-25 22:50:18 -0400

    Fixed command update

--- events/guild/messageUpdated.js
+++ events/guild/messageUpdated.js
@@ -34,7 +34,7 @@ function sleep(ms) {
 module.exports = async (client, message, newMsg) => {
     if (newMsg.data && newMsg.data.content && newMsg.type === "MessageUpdate") {
         const testCmd = client.commands.get(newMsg.data.content.slice(client.config.prefix.length)) || client.aliases.get(newMsg.data.content.slice(client.config.prefix.length));
-        if (!testCmd || testCmd.config.name === "ping") return;
+        if (!testCmd || testCmd && testCmd.config.name === "ping") return;
         client.emit("message", message)
     } else if (newMsg.type === "MessageReact") {
         const paginateCheck = client.paginate.get(newMsg.user_id);

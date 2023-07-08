1f138fd0ead18a41be54b6f1f198a6850682fbdf
ForGetFulSkyBro <forgetfulskybro9@gmail.com>
2023-07-08 12:59:01 -0400

    Updated credits

--- commands/credits.js
+++ commands/credits.js
@@ -1,5 +1,4 @@
 const Embed = require("../functions/embed.js");
-const translators = ["01H3FB6MTFN7WDV4T8ZTDTRV2K", "01GNAK3S9A677WSN41EG3GQAVC", "01FVB1ZGCPS8TJ4PD4P7NAFDZA", "01G9MCW5KZFKT2CRAD3G3B9JN5"];
 module.exports = {
     config: {
         name: "credits",
@@ -10,6 +9,8 @@ module.exports = {
         aliases: ["creds", "cred"]
     },
     run: async (client, message, args, db) => {
+        const translators = ["01H3FB6MTFN7WDV4T8ZTDTRV2K", "01GNAK3S9A677WSN41EG3GQAVC", "01FVB1ZGCPS8TJ4PD4P7NAFDZA", "01G9MCW5KZFKT2CRAD3G3B9JN5"];
+
         const embed = new Embed()
             .setTitle(client.translate.get(db.language, "Commands.credits.title"))
             .setDescription(`${client.translate.get(db.language, "Commands.credits.thankyou")}\n${client.translate.get(db.language, "Commands.credits.thankyou2")}\n\n**${client.translate.get(db.language, "Commands.credits.translators")}**:\n${translators.map(t => `<@${t}>`).join("\n")}`)

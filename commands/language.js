e9d9748ef0761f855b81071b4b01eb87622cea89
ForGetFulSkyBro <forgetfulskybro9@gmail.com>
2023-08-25 13:57:10 -0400

    Fix: wrong DB format

--- commands/language.js
+++ commands/language.js
@@ -10,14 +10,13 @@ module.exports = {
     },
     run: async (client, message, args, db) => {
         const embed = new Embed()
-            .setDescription(`### **Note**: Translations will be added once they are completed.\n\n**${client.translate.get(db.language, "Commands.language.current")}**: ${db.language}\n**${client.translate.get(db.language, "Commands.language.example")}**: ${db.prefix}language en_EN\n${client.translate.get(db.language, "Commands.language.change")}\n\n**${client.translate.get(db.language, "Commands.language.avail")}**: ${client.translate.availableLanguages.map(l => l).join(", ")}`)
+            .setDescription(`**${client.translate.get(db.language, "Commands.language.current")}**: ${db.language}\n**${client.translate.get(db.language, "Commands.language.example")}**: ${db.prefix}language en_EN\n${client.translate.get(db.language, "Commands.language.change")}\n\n**${client.translate.get(db.language, "Commands.language.avail")}**: ${client.translate.availableLanguages.map(l => l).join(", ")}`)
             .setColor(`#A52F05`);
 
         if (!args[0]) return message.reply({ embeds: [embed] }, false);
-
         if (!client.translate.availableLanguages.includes(args[0])) return message.reply(client.translate.get(db.language, "Commands.language.notAvailable"), false);
 
-        await db.updateOne({ language: args[0] });
+        await client.database.updateGuild(message.server.id, { language: args[0] });
         message.reply(`${client.translate.get(args[0], "Commands.language.success")} **${args[0]}**`, false);
     },
 };
